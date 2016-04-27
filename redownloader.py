4ae5b93fa5624912b7fd131ef7c06a4bdc3aa4b4
Will Wagner <willwagner602@gmail.com>
2016-04-27 09:16:43 -0700

    - Randomized the image list for downloads, makes it easier to retrieve short (50-100) sets of different images

--- redownloader.py
+++ redownloader.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import sqlalchemy
 
@@ -17,7 +18,6 @@ def download_aircraft_images(aircraft, engine, image_limit=50):
     aircraft_page = 'https://commons.wikimedia.org/wiki/Category:Aircraft_by_location_by_aircraft_type'
     image_directory = '/home/wbw/pycharmprojects/wiki-subcatscraper/Images/{}'
 
-    aircraft = 'Airbus A380'
     select_images = """ SELECT
                           image_id,
                           image_url,
@@ -31,9 +31,10 @@ def download_aircraft_images(aircraft, engine, image_limit=50):
 
     images = execute_db_query(select_images, engine)
 
-    print(images)
+    # randomize images so it's not all just the same plane
+    random.shuffle(images)
 
-    for image in images:
+    for image in images[:image_limit]:
         image_id = image[0]
         image_url = image[1]
         image_name = image[2]
@@ -43,7 +44,7 @@ def download_aircraft_images(aircraft, engine, image_limit=50):
 
         print('Downloading image {} with image id {}'.format(image_name, image_id))
 
-        print(download_image(image_directory.format(image_location), image_name, image_url))
+        download_image(image_directory.format(image_location), image_name, image_url)
 
         update_image = """UPDATE
                             images
@@ -66,7 +67,15 @@ def download_useful_images(engine):
                         FROM
                           images
                         WHERE
-                          use_flag = 1"""
+                          use_flag = 1
+                          and redownload_flag = 1"""
+
+    update_image = """UPDATE
+                            images
+                          SET
+                            redownload_flag = 0
+                          WHERE
+                            image_id = {}"""
 
     images = execute_db_query(useful_images, engine)
 
@@ -79,6 +88,8 @@ def download_useful_images(engine):
         print('Downloading image {} with image id {}'.format(image_name, image_id))
 
         download_image(image_directory.format(image_location), image_name, image_url)
+
+        engine.execute(update_image.format(image_id))
 
 
 def create_folders(engine):
@@ -100,23 +111,34 @@ def create_folders(engine):
 
     engine.dispose()
 
-if __name__ == '__main__':
 
-    # setup SQL alchemy connection
-    engine = sqlalchemy.create_engine('sqlite:////home/wbw/Dropbox/Programming/Projects/PlaneViewer/images.sqlite3')
+def download_representative_images(engine):
 
-    download_useful_images(engine)
-
-    aircraft_select = """SELECT
-          DISTINCT
-          aircraft
-        FROM
-          images"""
+    aircraft_select = """
+        SELECT aircraft FROM images
+        WHERE redownload_flag = 1 AND aircraft IS NOT NULL AND aircraft IN (
+        SELECT aircraft FROM images
+        WHERE aircraft IS NOT NULL
+        GROUP BY aircraft
+        HAVING SUM(CASE WHEN redownload_flag = 1 THEN 0 WHEN redownload_flag = 0 THEN 1 END) < 20)
+        GROUP BY aircraft"""
 
     aircraft_list = execute_db_query(aircraft_select, engine)
 
     for aircraft in aircraft_list:
         aircraft = aircraft[0]
         download_aircraft_images(aircraft, engine)
+
+
+if __name__ == '__main__':
+
+    # setup SQL alchemy connection
+    engine = sqlalchemy.create_engine('sqlite:////home/wbw/Dropbox/Programming/Projects/PlaneViewer/images.sqlite3')
+
+    download_aircraft_images('Boeing 747', engine)
+
+    # create_folders(engine)
+    # download_useful_images(engine)
+    # download_representative_images(engine)
 
     engine.dispose()
